fb5dee29d9ba1f343df7eb424f7008b895367513
P-Buranakitsin <pavaruth.buranakitsin@gmail.com>
2020-09-04 13:41:34 +0700

    third commit

--- mathquiz.go
+++ mathquiz.go
@@ -7,14 +7,55 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
-
 func main() {
-	filePtr, _, countPtr := comFlag()
-	countAndRead(filePtr, countPtr)
+	filePtr, timePtr, countPtr := comFlag()
+	indexVal, numLine := readFile(filePtr)
+	randLine := genRandLine(countPtr, numLine)
 
+	var correctAns int // Number of correct answer
+	c1 := make(chan string)
+
+	var start time.Time
+	var duration time.Duration
+	// Start another go routine
+	go func() {
+		start = time.Now() // Measure execution time
+		// Randomly select lines from a file by using map
+		for k := range randLine {
+			split := strings.Split(indexVal[k], ",") // split into slice of strings, index 0 is question while index 1 is answer
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print(split[0], " = ")
+
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				panic(err)
+			}
+
+			// string returned from reader.ReadString includes Carriage Return (ASCII 13) and Line Feed (ASCII 10)
+			// Slicing is used to remove Carriage Return and Line Feed
+			if text[0:len(text)-2] == split[1] {
+				correctAns++
+			}
+		}
+		duration = time.Since(start)
+		c1 <- "Done"
+	}()
+
+	// Select blocks until either case is satisfied
+	select {
+	case <-c1:
+		fmt.Println("correct =", correctAns)
+		fmt.Println("time used =", duration.Seconds(), "seconds")
+	case <-time.After(time.Duration(*timePtr) * time.Second):
+		fmt.Println()
+		fmt.Println("Time's up!!!!")
+		fmt.Println("correct =", correctAns)
+		fmt.Println("time used =", *timePtr, "seconds")
+	}
 }
 
 // Command-Line Flags
@@ -22,7 +63,7 @@ func comFlag() (*string, *int, *int) {
 	//file flag
 	filePtr := flag.String("file", "./questions.csv", "file path to read from")
 	//timeout flag
-	timePtr := flag.Int("timeout", 180, "duration in seconds for each question")
+	timePtr := flag.Int("timeout", 180, "duration in seconds to answer all questions")
 	//count flag
 	countPtr := flag.Int("count", 5, "number of questions")
 
@@ -30,9 +71,43 @@ func comFlag() (*string, *int, *int) {
 	return filePtr, timePtr, countPtr
 }
 
+// Read from a file
+func readFile(filePtr *string) (map[int]string, int) {
+
+	f, err := os.Open(*filePtr)
+	var numLine int
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	s := bufio.NewScanner(f)
+
+	var indexVal = map[int]string{} // Create map to store string read in each line as well as its no. of line
+	for s.Scan() {
+		indexVal[numLine] = s.Text()
+		numLine++ // numLine count no. of lines from a file
+	}
+	err = s.Err() // Check an error while scanning
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return indexVal, numLine
+}
+
 // Generate unique random numbers of lines to read from
 func genRandLine(countPtr *int, numLine int) map[int]bool {
 	randLine := map[int]bool{}
+
+	if *countPtr > numLine {
+		*countPtr = numLine
+	}
 
 	i := 0
 	for i != *countPtr {
@@ -45,42 +120,4 @@ func genRandLine(countPtr *int, numLine int) map[int]bool {
 	}
 
 	return randLine
-}
-
-// Count lines from a file and read random lines from a file
-func countAndRead (filePtr *string, countPtr *int) {
-	// Count lines from a file
-	f, err := os.Open(*filePtr)
-	var numLine int
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer func() {
-        if err = f.Close(); err != nil {
-        log.Fatal(err)
-    }
-	}()
-	
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		numLine++
-	}
-	err = s.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// After counting lines, select random lines from a file
-	randLine := genRandLine(countPtr, numLine)
-	fmt.Println(randLine)
-	f.Seek(0,0)
-	s = bufio.NewScanner(f)// Reset Scanner 
-	var line int
-	for s.Scan() {
-		if (randLine[line]) {
-			fmt.Println(s.Text())
-		}
-		line++
-	}
 }
